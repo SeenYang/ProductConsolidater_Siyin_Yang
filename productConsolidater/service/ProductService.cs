fffff7d1f2b068b822ed06e2ceb5b5cedd2c8879
yangsiyin999 <siyin.yang@carsales.com.au>
2021-02-13 22:09:39 +1100

    Update ProductService.cs

--- productConsolidater/service/ProductService.cs
+++ productConsolidater/service/ProductService.cs
@@ -1,4 +1,6 @@
-﻿using productConsolidater.model;
+﻿using System;
+using System.Linq;
+using productConsolidater.model;
 
 namespace productConsolidater.service
 {
@@ -12,13 +14,30 @@ namespace productConsolidater.service
         /// <summary>
         /// Steps for processing products:
         /// 1. Product barcode is provided by manufacturers, which is unique.
-        /// 2. When two line contain same barcode, take the line with smaller source Id (company id)
-        /// 3. 
+        /// 2. When two lines contain same barcode, take the line with smaller source Id (company id)
+        /// 3. Group data by SKU (internal product code), Source Id (Company)
         /// </summary>
         /// <param name="mockContext"></param>
         /// <exception cref="NotImplementedException"></exception>
         public void ProcessProduct(MockDbContextDto mockContext)
         {
+            var temp = (
+                from bc in mockContext.Barcodes
+                group bc by bc.Barcode
+                into groupList
+                select new
+                {
+                    barcode = groupList.Key,
+                    Skus = groupList.Select(g => 
+                        new
+                        {
+                            g.Sku, 
+                            g.SupplierId, 
+                            g.DataSourceId
+                        })
+                }).ToList();
+            
+
             throw new System.NotImplementedException();
         }
     }
